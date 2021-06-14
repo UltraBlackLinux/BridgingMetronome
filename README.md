95edcee6ced870ca7731b1ea723896bec1bc7e46
UltraBlackLinux <accoundshit@protonmail.com>
2021-06-14 20:34:40 +0200

    - 1.17 and license

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-[![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)](http://perso.crans.org/besson/LICENSE.html)
-
-[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
-
 # BridgingMetronome
 Helps you find the right rhythm for bridging
 __This mod is compatible with any Version of Fabric!__
--- src/main/java/me/ultrablacklinux/bridgingmetronome/BridgingMetronome.java
+++ src/main/java/me/ultrablacklinux/bridgingmetronome/BridgingMetronome.java
@@ -5,7 +5,7 @@ import me.ultrablacklinux.bridgingmetronome.util.MetroThreadHelper;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.options.KeyBinding;
+import net.minecraft.client.option.KeyBinding;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
